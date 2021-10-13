3af8b410143667b52e2a9eca4197df1edf780bc4
Titoxox97 <87781987+Titoxox97@users.noreply.github.com>
2021-10-13 19:00:36 -0400

    revamp html

--- mySquad.js
+++ mySquad.js
@@ -1,3 +1,5 @@
+const Manager = require("./lib/Manager");
+
 const createmySquadHTML = (team) => {
   const createEngineer = (engineer) => {
     return `<div class="col-5">
@@ -6,12 +8,41 @@ const createmySquadHTML = (team) => {
         <ul class="list-group list-group-flush">
             <li class="list-group-item">ID: ${engineer.id}</li>
             <li class="list-group-item">Email Address: ${engineer.email}</li>
-            
+            <li class="list-group-item">Email Address: ${engineer.github}</li>
         </ul>
         </div>
     </div>
         `;
   };
+
+  const createManager = (manager) => {
+    return `<div class="col-5">
+    <div class="card mx-auto mb-3" style="max-width: 20rem">
+    <h5 class="card-header">${manager.name}<br /><br />Engineer</h5>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${manager.id}</li>
+        <li class="list-group-item">Email Address: ${manager.email}</li>
+        <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+    </ul>
+    </div>
+</div>
+    `;
+  };
+
+  const createIntern = (intern) => {
+    return `<div class="col-5">
+    <div class="card mx-auto mb-3" style="max-width: 20rem">
+    <h5 class="card-header">${intern.name}<br /><br />Engineer</h5>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${intern.id}</li>
+        <li class="list-group-item">Email Address: ${intern.email}</li>
+        <li class="list-group-item">School: ${intern.school}</li>
+    </ul>
+    </div>
+</div>
+    `;
+  };
+
   console.log(team);
   return `
     <!DOCTYPE html>
@@ -27,58 +58,9 @@ const createmySquadHTML = (team) => {
     </head>
 
     <body>
-        <div class="container container-fluid text-center">
-            <div class="jumbotron bg-danger">
-                <h1 class="bg-danger text-white">My Team</h1>
-            </div>
-            <div class = "row">
-      
-        </div>
-        <div class="col-5">
-            <div class="card mx-auto mb-3" style="max-width: 20rem">
-            <h5 class="card-header">${data[i].name}<br /><br />${Engineer}</h5>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${data[i].id}</li>
-                <li class="list-group-item">Email Address: ${data[i].email}</li>
-                
-            </ul>
-            </div>
-        </div>
+    <div id = content>
 
-        <div class="col-5">
-            <div class="card mx-auto mb-3" style="max-width: 20rem">
-            <h5 class="card-header">${data[i].name}<br /><br />${Manager}</h5>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${data[i].id}</li>
-                <li class="list-group-item">Email Address: ${data[i].email}</li>
-                
-            </ul>
-            </div>
-
-            <div class="col-5">
-                <div class="card mx-auto mb-3" style="max-width: 20rem">
-                <h5 class="card-header">${data[i].name}<br /><br />${Intern}</h5>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${data[i].id}</li>
-                    <li class="list-group-item">Email Address: ${data[i].email}</li>
-                    
-                </ul>
-                </div>
-            </div>
-
-            <div class="col-5">
-                <div class="card mx-auto mb-3" style="max-width: 20rem">
-                <h5 class="card-header">${data[i].name}<br /><br />${Employee}</h5>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${data[i].id}</li>
-                    <li class="list-group-item">Email Address: ${data[i].email}</li>
-                    
-                </ul>
-                </div>
-            </div>
-        </div>
     </body>
-
     </html>`;
 };
 module.exports = createmySquadHTML;
